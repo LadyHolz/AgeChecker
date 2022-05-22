488fbf47819395563b49f9d5324f845f803cbe5a
Gemma Holz <Gemma.Holz@balticapprenticeships.com>
2022-05-22 13:01:54 +0100

    Add project files.

---
+++ AgeChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AgeChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age > 65)
+            {
+                Console.WriteLine("Your ticket price is £4!");
+            }
+            else if (age <= 18)
+            {
+                Console.WriteLine("Your ticket price is £4.50!");
+            }
+            else
+            {
+                Console.WriteLine("Your ticket price £6.50");
+            }
+
+        }
+    }
+}
